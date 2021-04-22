d2d1d1095c6dae451e83c946db6a2c587d2a2e8e
Adriana Biberg <adribiberg@hotmail.com>
2021-04-22 19:32:13 -0300

    Adiciona exercicio 3 e corrige os anteriores

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/operadoresAritimeticos.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/operadoresAritimeticos.js
@@ -1,5 +1,5 @@
 let a = 12;
-let b = 5;
+let b = 70;
 
 // Adição
 console.log(a+b);
@@ -18,8 +18,25 @@ console.log(a%b);
 
 // Retorne o maior de dois números
 
-if (a > b) {
-    console.log(a);
+let z = 4;
+let y = 12;
+
+if (z > y) {
+    console.log(z);
 } else {
-    console.log(b);
+    console.log(y);
+}
+
+// Retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+
+let c = 10;
+let d = 30;
+let e = 18;
+
+if (c > d && c > e) {
+    console.log(c);
+} else if (d > e) {
+    console.log(d);
+}else {
+    console.log(e);
 }
