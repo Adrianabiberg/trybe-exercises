0fd06efc07cd89fd3eb8995c5feb78347f641dbd
Adriana Biberg <adribiberg@hotmail.com>
2021-04-25 12:58:02 -0300

    Resolve exercicio 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/operadoresAritimeticos.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/operadoresAritimeticos.js
@@ -42,3 +42,13 @@ if (c > d && c > e) {
 }
 
 // dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+let f = -7;
+
+if (f > 0) {
+    console.log("positive");
+} else if (f < 0){
+    console.log("negative");
+} else {
+    console.log("zero");
+}
