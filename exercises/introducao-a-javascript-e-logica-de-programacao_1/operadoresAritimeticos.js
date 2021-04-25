4d35aaea2ea8545a1738928641f7c558cdd64f42
Adriana Biberg <adribiberg@hotmail.com>
2021-04-25 20:16:39 -0300

    Resolve exercicio 9

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/operadoresAritimeticos.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/operadoresAritimeticos.js
@@ -147,3 +147,19 @@ if (num1%2 === 0 || num2%2 ===0 || num3%2 ===0) {
 };
 
 console.log("=================================================");
+
+// Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+// Bonus: use somente um if.
+
+let num4 = 8;
+let num5 = 55;
+let num6 = 28;
+
+if (num4%2 === 1 || num5%2 === 1 || num6%2 === 1) {
+    console.log("true");
+} else {
+    console.log("false");
+};
+
+console.log("=================================================");
+
