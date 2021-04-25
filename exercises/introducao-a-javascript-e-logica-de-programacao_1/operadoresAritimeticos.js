b7330c4df9b84398876f527562941b105de162e2
Adriana Biberg <adribiberg@hotmail.com>
2021-04-25 14:09:56 -0300

    Resolve exercicio 7

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/operadoresAritimeticos.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/operadoresAritimeticos.js
@@ -102,3 +102,29 @@ if ( pLowerCase === "dama") {
 }
 
 console.log("=================================================");
+
+// Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+// Porcentagem >= 90 -> A
+// Porcentagem >= 80 -> B
+// Porcentagem >= 70 -> C
+// Porcentagem >= 60 -> D
+// Porcentagem >= 50 -> E
+// Porcentagem < 50 -> F
+
+let nota = 49;
+
+if (nota >= 90) {
+    console.log("nota: A");
+} else if (nota >= 80 && nota < 90) {
+    console.log("nota: B");
+} else if (nota >= 70 && nota < 80) {
+    console.log("nota: C");
+} else if (nota >= 60 && nota < 70) {
+    console.log("nota: D");
+} else if (nota >= 50 && nota < 60) {
+    console.log("nota: E");
+} else {
+    console.log("nota: F");
+}
+
+console.log("=================================================");
