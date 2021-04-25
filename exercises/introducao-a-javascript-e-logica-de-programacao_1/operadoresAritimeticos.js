46556b1879434345f6d128f9382cce73c96bf37a
Adriana Biberg <adribiberg@hotmail.com>
2021-04-25 13:22:11 -0300

    Incrementa o exercicio 5

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/operadoresAritimeticos.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/operadoresAritimeticos.js
@@ -55,13 +55,14 @@ if (f > 0) {
 
 // defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
 
-let anguloA = 35;
+let anguloA = 55;
 let anguloB = 55;
 let anguloC = 90;
 
 if (anguloA + anguloB + anguloC === 180){
     console.log("true");
+} else if (anguloA < 0 || anguloB < 0 || anguloC < 0){
+    console.log("erro. Valores inválidos, verique se o valor é positivo");
 } else {
     console.log("false");
-    console.log("Erro. Verifique os valores inseridos.");
 }
