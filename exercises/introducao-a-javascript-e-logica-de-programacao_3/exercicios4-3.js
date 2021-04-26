59a82e9496eac089b5d376743ff5b288e93b0b50
Adriana Biberg <adribiberg@hotmail.com>
2021-04-26 18:40:04 -0300

    Resolve exercicios 1 e 2

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicios4-3.js
@@ -0,0 +1,22 @@
+let n = 9;
+let asterisco = "";
+
+for (let linha = 1; linha <= n; linha += 1) {
+    asterisco += "*";
+};
+
+for (let coluna = 0; coluna < n; coluna += 1) {
+    console.log(asterisco);
+};
+
+console.log("=========================================");
+
+let x = 6;
+let asterisco2 = "";
+
+for (let tri = 1; tri <= x; tri += 1) {
+    asterisco2 += "*";
+    console.log(asterisco2);
+};
+
+console.log("=========================================");
