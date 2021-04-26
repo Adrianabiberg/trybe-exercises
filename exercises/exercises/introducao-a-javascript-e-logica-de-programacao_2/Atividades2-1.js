0f7222d0883d4d95332673e27e55fe31e8dfdb6e
Adriana Biberg <adribiberg@hotmail.com>
2021-04-26 11:05:56 -0300

    Resolve exercicio 2

--- exercises/exercises/introducao-a-javascript-e-logica-de-programacao_2/Atividades2-1.js
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_2/Atividades2-1.js
@@ -2,5 +2,16 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let index = 0; index < numbers.length; index += 1) {
     console.log(numbers[index]);
-}
+};
 
+console.log("=============================================");
+
+let somaTotal = 0;
+
+for (let soma = 0; soma < numbers.length; soma +=1) {
+ somaTotal += numbers[soma];
+};
+
+console.log(somaTotal);
+
+console.log("=============================================");
