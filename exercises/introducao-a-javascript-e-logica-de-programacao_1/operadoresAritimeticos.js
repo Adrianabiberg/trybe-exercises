dc1b6840c339d2c353ad907f2be6e6a75eaba9db
Adriana Biberg <adribiberg@hotmail.com>
2021-04-25 21:51:45 -0300

    Resolve exercicio 11 e adiciona texto ao README

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/operadoresAritimeticos.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/operadoresAritimeticos.js
@@ -184,3 +184,49 @@ if (custo < 0 || venda < 0) {
 };
 
 console.log("=================================================");
+
+// Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+
+let salarioBruto = 4664.98;
+let salarioBase = 0;
+let salarioLiquido = 0;
+
+console.log("Com base no salário bruto de R$" + salarioBruto + " segue:");
+
+if (salarioBruto <= 1556.94) {
+    salarioBase = salarioBruto - (salarioBruto * 0.08);
+    console.log("Desconto do INSS de 8%");
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+    salarioBase = salarioBruto - (salarioBruto * 0.09);
+    console.log("Desconto do INSS de 9%");
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+    salarioBase = salarioBruto - (salarioBruto * 0.11);
+    console.log("Desconto do INSS de 11%");
+} else {
+    salarioBase = salarioBruto - 570.88;
+    console.log("Desconto do INSS de R$570,88");
+};
+
+if (salarioBase <= 1903.98) {
+    salarioLiquido = salarioBase;
+    console.log("Salário líquido é de: R$" + salarioLiquido);
+    console.log("Não há dedução de IR.");
+} else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+    salarioLiquido = salarioBase - (salarioBase * 0.075 - 142.80);
+    console.log("IR de 7,5% menos R$ 142,80");
+    console.log("Salário líquido é de: R$" + salarioLiquido);
+} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+    salarioLiquido = salarioBase - (salarioBase * 0.15 - 354.80);
+    console.log("IR de 15% menos R$ 354,80");
+    console.log("Salário líquido é de: R$" + salarioLiquido);
+} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+    salarioLiquido = salarioBase - (salarioBase * 0.225 - 636.13);
+    console.log("IR de 22,5% menos R$ 636,13");
+    console.log("Salário líquido é de: R$" + salarioLiquido);
+} else if (salarioBase >= 4664.69) {
+    salarioLiquido = salarioBase - (salarioBase * 0.275 - 869.36);
+    console.log("IR de 27,5% menos R$ 869,36");
+    console.log("Salário líquido é de: R$" + salarioLiquido);
+} else {
+    console.log("insira valores válidos.");
+};
