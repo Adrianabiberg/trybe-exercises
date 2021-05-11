fa5d3253d0218096b046a0dc08398680633fba3c
Adriana Biberg <adribiberg@hotmail.com>
2021-05-11 19:01:14 -0300

    Adiciona exercicio 1

---
+++ exercises/exercises/html-e-css-forms-flexbox-e-responsivo.1/script.js
@@ -0,0 +1,9 @@
+let states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás',  'Maranhão', 'Mato Grosso',' Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+
+const statesBox = document.querySelector('#states');
+
+for (let index = 0; index < states.length; index += 1) {
+    let options = document.createElement("option");
+    options.innerText = states[index];
+    statesBox.appendChild(options);
+}
