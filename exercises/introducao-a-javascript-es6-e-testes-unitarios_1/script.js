b2de7e5371dbb7b1c0d1afdac7866114745fd0da
Adriana Biberg <adribiberg@hotmail.com>
2021-05-19 19:27:37 -0300

    Finaliza o exercicio 2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
@@ -13,3 +13,27 @@ const testingScope = (escopo) => {
     }
   }
   testingScope(true);
+
+// 2.  Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+// Copie o código abaixo.
+
+let oddsAndEvens = [13, 3, 4, 10, 7, 2];
+// Seu código aqui.
+
+oddsAndEvens.sort((a, b) => {
+    if(a > b) {
+      return 1;
+    }
+    if (a < b) {
+      return -1;
+    }
+   return 0;
+});
+
+oddsAndEvens = `Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`;
+
+console.log(oddsAndEvens);
+
+// usei como referência para esse exercício o site: https://ricardo-reis.medium.com/o-m%C3%A9todo-sort-do-array-javascript-482576734e0a#:~:text=Por%20padr%C3%A3o%2C%20o%20m%C3%A9todo%20sort,para%20determinar%20as%20suas%20ordens 
