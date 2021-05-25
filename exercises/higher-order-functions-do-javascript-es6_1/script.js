89f4a8709976d90dab1b354e25511c8253304dbc
Adriana Biberg <adribiberg@hotmail.com>
2021-05-25 20:05:11 -0300

    Exercicio 3

--- exercises/higher-order-functions-do-javascript-es6_1/script.js
+++ exercises/higher-order-functions-do-javascript-es6_1/script.js
@@ -35,4 +35,35 @@ const sorteioResults = (num, callback) => {
 
 const sorteio = () => { parseInt(Math.random() * (5 - 1) + 1); };
 
-console.log(sorteioResults(2, sorteio));
+// console.log(sorteioResults(2, sorteio));
+
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas 
+// (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa 
+// estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da 
+// pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e 
+// quando não houver resposta ("N.A") não altera-se a contagem.
+
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const correcao = (gabarito, respEstudante, callback) => {
+   return callback(gabarito, respEstudante);
+};
+
+const botcorretor = (gabarito, respEstudante) => {
+  let nota = 0;
+  for (let index = 0; index < gabarito.length; index += 1) {
+    if (respEstudante[index] === 'N.A') {
+      nota += 0;
+    } else if (respEstudante[index] === gabarito[index]){
+      nota += 1;
+    } else {
+      nota -= 0.5;
+    }
+  }
+  return `A pontuação total foi de ${nota} pontos`;
+}
+
+console.log(correcao(rightAnswers, studentAnswers, botcorretor));
+// Questão respondida com ajuda do colega Rodrigo Facury;
