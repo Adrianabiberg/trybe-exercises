0094a43f96a6c54b18fac545c792408fb45ca780
Adriana Biberg <adribiberg@hotmail.com>
2021-05-25 19:03:22 -0300

    Exercicio 2

--- exercises/higher-order-functions-do-javascript-es6_1/script.js
+++ exercises/higher-order-functions-do-javascript-es6_1/script.js
@@ -16,7 +16,23 @@ const createData = (nomeCompleto) => {
   return {
     nomeCompleto,
     email: `${nomeCompleto}@trybe.com`.toLowerCase().replace(' ', '_'),
-  }
-}
+  };
+};
 
-console.log(newEmployees(createData));
+newEmployees(createData);
+
+// 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número 
+// aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se 
+// o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string 
+// (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const sorteioResults = (num, callback) => {
+  if (callback() === num) {
+    return 'Parabéns você ganhou';
+  }
+  return 'Tente novamente';
+};
+
+const sorteio = () => { parseInt(Math.random() * (5 - 1) + 1); };
+
+console.log(sorteioResults(2, sorteio));
